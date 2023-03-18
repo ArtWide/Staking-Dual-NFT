b577f7676047d5e84add9e2346a3c6eb5451f0a7
Artwide <rkafkko@gmail.com>
2023-03-18 18:49:42 +0900

    edit

--- contract-v2/module/RewardVault.sol
+++ contract-v2/module/RewardVault.sol
@@ -10,7 +10,6 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 
 contract NTSRewardVault is PermissionsEnumerable {
