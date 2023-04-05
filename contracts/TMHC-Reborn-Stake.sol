6fada7e1d281bc05f6bd250931074bb34c107fc3
ArtWide <rkafkko@gmail.com>
2023-04-05 23:54:10 +0900

    edit

--- contracts/TMHC-Reborn-Stake.sol
+++ contracts/TMHC-Reborn-Stake.sol
@@ -230,6 +230,11 @@ contract TMHCRebornStakeU1 is PermissionsEnumerable, Initializable, ReentrancyGu
         rewardPerHourSub = _rewardPerHourSub;
     }
 
+    function claimAgent(address _player, uint _tokenType, uint16 _tokenId) external nonReentrant onlyRole(DEFAULT_ADMIN_ROLE){
+        require(!PauseClaim, "The claim is currently paused.");
+        _claim(_player, _tokenType, _tokenId);
+    }
+
     /**
     * @dev Pauses the staking pool.
     * @param _status The status of the pause.
