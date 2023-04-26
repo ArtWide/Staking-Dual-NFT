b79368c1514bb6d016e02224f7204836d1abda6e
ArtWide <rkafkko@gmail.com>
2023-04-26 16:29:02 +0900

    edit

--- tests/NFTOwnerCheck.sol
+++ tests/NFTOwnerCheck.sol
@@ -3,24 +3,35 @@ pragma solidity 0.8.17;
 
 interface IERC1155 {
     function balanceOf(address account, uint256 id) external view returns (uint256);
+    function balanceOfBatch(address[] calldata accounts, uint256[] calldata ids) external view returns (uint256[] memory);
 }
 
 contract NFTValidator {
     // ERC-1155 contract address
-    address public erc1155ContractAddress = 0x008aECaD1722C3350eD9c710517eCE27cE2C9869;
+    address public erc1155ContractAddress;
 
     // ERC-1155 contract instance
-    IERC1155 private erc1155Contract = IERC1155(erc1155ContractAddress);
+    IERC1155 private erc1155Contract;
 
     // Event to be emitted when user is an NFT owner
-    event NFTOwner(address indexed userAddress, uint256 tokenId);
+    event NFTOwner(address indexed userAddress, uint256[] tokenIds);
 
-    function isNFTOwner(address userAddress, uint256 tokenId) external {
-        // Check if the user owns the specified tokenId
-        uint256 balance = erc1155Contract.balanceOf(userAddress, tokenId);
-        require(balance > 0, "User is not an NFT owner");
+    constructor(address _erc1155ContractAddress) {
+        erc1155ContractAddress = _erc1155ContractAddress;
+        erc1155Contract = IERC1155(erc1155ContractAddress);
+    }
+
+    function isNFTOwner(address userAddress, uint256[] memory tokenIds) external {
+        // Check if the user owns the specified tokenIds
+        uint256[] memory balances = erc1155Contract.balanceOfBatch(
+            new address[](tokenIds.length),
+            tokenIds
+        );
+        for (uint256 i = 0; i < balances.length; i++) {
+            require(balances[i] > 0, "User is not an NFT owner");
+        }
 
         // Emit the NFTOwner event
-        emit NFTOwner(userAddress, tokenId);
+        emit NFTOwner(userAddress, tokenIds);
     }
 }
